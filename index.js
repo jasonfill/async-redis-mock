a83c4e8607e0cb7901f0ea0f95b953cf563ca0e3
Jason Fill <jasonfill@gmail.com>
2023-01-16 08:42:19 -0500

    Fixing bug

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const redis = require('redis-mock');
+const redisMock = require('redis-mock');
 const rewire = require('rewire');
 
 const asyncRedis = rewire('async-redis');
 
 // eslint-disable-next-line no-underscore-dangle
 asyncRedis.__set__({
-  redis,
+  redisMock,
 });
 
 module.exports = asyncRedis;
